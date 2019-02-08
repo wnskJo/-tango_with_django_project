3dd8d286111236125781f6a801af3c1a8de60565
Wonseok Cho <wonseokcho777@gmail.com>
2019-02-08 09:51:22 +0000

    Chapter 1-rev 1

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")
+    return HttpResponse("rango says here is the about page. <a href='/rango/'>Index</a>.")
